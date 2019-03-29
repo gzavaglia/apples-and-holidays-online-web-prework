5a437fa81c73abfe15cf332a3565b502f257b6c5
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-29 16:28:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |key, value|
+puts "#{key.capitalize}: "
+value.each do |key2, value2|
+puts "#{key2.capitalize}: #{value2.join(", ")}"
+end 
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
