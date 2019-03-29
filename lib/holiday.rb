d4c9b0bb31cf36fc151830fd61a66c4159b0dc92
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-29 16:30:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,8 +65,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |key, value|
-puts "#{key.capitalize}:"
+
 value.each do |key2, value2|
+  puts "#{key.capitalize}:"
 puts "#{key2}: #{value2.join(", ")}"
 end 
 end
