7cb92d9ac15d592101babfcd2a7f5ac773b418c4
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-29 17:05:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,16 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+bbq_key = nil 
+bbq_value = nil 
 
+holiday_hash.each do |k,v|
+  if v == "BBQ"
+    bbq_value = v 
+    bbq_key = k
+  end # if 
+end #do 
+bbq_key
 end
 
 
